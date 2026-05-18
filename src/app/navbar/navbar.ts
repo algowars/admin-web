cbc92b9acd5cc3e2c2a06d0e3de3a204d9fde1f4
admclamb <adylanmclamb@yahoo.com>
2026-05-17 20:27:19 -0400

    fix navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,9 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, Signal, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [MenubarModule],
   templateUrl: './navbar.html',
 })
-export class Navbar {}
+export class Navbar {
+  private readonly auth = inject(AuthService);
+
+  private readonly isAuthenticated = toSignal(this.auth.isAuthenticated$);
+
+  private readonly baseMenuItems = signal([
+    {
+      label: 'Home',
+      icon: 'pi pi-home',
+      routerLink: '/',
+    },
+  ]).asReadonly();
+
+  private readonly authMenuItems = signal([
+    {
+      label: 'Profile',
+      icon: 'pi pi-user',
+      routerLink: '/profile',
+    },
+    {
+      label: 'Logout',
+      icon: 'pi pi-sign-out',
+      command: () => this.logout(),
+    },
+  ]).asReadonly();
+
+  private readonly unauthMenuItems = computed(() => [
+    {
+      label: 'Login',
+      icon: 'pi pi-sign-in',
+      command: () => this.login(),
+    },
+  ]);
+
+  readonly menuItems: Signal<MenuItem[]> = computed(() => [
+    ...this.baseMenuItems(),
+    ...(this.isAuthenticated() ? this.authMenuItems() : this.unauthMenuItems()),
+  ]);
+
+  login() {
+    this.auth.loginWithRedirect();
+  }
+
+  logout() {
+    this.auth.logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  }
+}
